dd7c87ef0f7890b01af14a4c51e9c3291b2590ba
Maksim Loginov <loginovmi@gmail.com>
2017-05-16 19:17:56 +1000

    first commit

---
+++ battleship/battleship.js
@@ -0,0 +1,31 @@
+var location1 = 3,
+	location2 = 4,
+	location3 = 5,
+	guess,
+	hits = 0,
+	guesses = 0,
+	isSunk = false;
+
+while (isSunk == false) { // пока корабль не потоплен
+	guess = prompt('Ready, aim, fire! (enter a number 0 - 6):');
+	if (guess < 0 || guess > 6) { // проверяем предположение пользователя
+		alert('Please enter a valid cell number!');
+	} else {
+		guesses++; // если введенное значение корректно, увеличиваем счетчик попыток
+
+		if (guess == location1 || guess == location2 || guess == location3) {
+			alert('HIT!');
+			hits++; // если есть попадание, то увеличиваем счетчик попаданий
+
+			if (hits == 3) {
+				isSunk = true;
+				alert('You sunk my battleship!');
+			}
+		} else {
+			alert('MISS!');
+		}
+	}
+}
+
+var stats = 'You took ' + guesses + ' guesses to sink the battleship, ' + 'wich means your shooting accuracy was ' + (3/guesses);
+alert(stats);
