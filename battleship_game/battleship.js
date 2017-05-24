fa6baef4c4bd3d25043a0e62b09065e268d80cb1
Maksim Loginov <loginovmi@gmail.com>
2017-05-24 22:10:16 +1000

    Added several lines to the model object

--- battleship_game/battleship.js
+++ battleship_game/battleship.js
@@ -49,17 +49,46 @@ var model = {
     fire: function (guess) {
         for (var i = 0; i < this.numShips; i++) {
             var ship = this.ships[i];
-            var locations = ship.locations;
             // если координаты клетки присутсивуют в массиве, то выстрел попал в цель
-            var index = locations.indexOf(guess);
+            var index = ship.locations.indexOf(guess);
             // в массиве hits помечаем попадание, значение = index
             if (index >= 0) {
                 ship.hits[index] = 'hit';
+                // оповещаем представление о том, что нужно пометить маркером попадание в корабль в клетке guess
+                view.displayHit(guess);
+                // говорим представлению, чтобы нужно вывести сообщение 'HIT!'
+                view.displayMessage('HIT!');
+                //если корабль потоплен, то увеличиваем счетчик потопленных кораблей в свойстве shipsSunk модели
+                if (this.isSunk(ship)) {
+                    // приказываем представлению вывести сообщение для игрока, что он потопил корабль
+                    view.displayMessage('You sank my battleship!');
+                    this.shipsSunk++;
+                }
                 // истина возвращается, при удачном выстреле
                 return true;
             }
         }
+        // сообщаем представлению, что нужно вывести маркер промаха в клетке guess
+        view.displayMiss(guess);
+        // приказываем представлению вывести сообщение о промахе
+        view.displayMessage('You missed! Ha-ha!');
+        // если после перебора всех кораблей попадание не обнаружено, то игрок промахнулся, а метод возвращает false
         return false;
+    },
+    /**
+     * метод проверки потоплен ли корабль
+     * @param ship
+     * @returns {boolean}
+     */
+    isSunk: function (ship) {
+        // если есть хотя бы одна клетка, которая не помечена 'hit', то корабль еще не потоплен и возвращаем ложь
+        for (var i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== 'hit') {
+                return false;
+            }
+        }
+        // если корабль потоплен, то возвращаем истину
+        return true;
     }
 
 };
@@ -71,3 +100,14 @@ var model = {
 // view.displayMiss("25");
 // view.displayHit("26");
 // view.displayMessage("Tap tap, is this thing on?");
+
+model.fire("53");
+model.fire("06");
+model.fire("16");
+model.fire("26");
+model.fire("34");
+model.fire("24");
+model.fire("44");
+model.fire("12");
+model.fire("11");
+model.fire("10");
