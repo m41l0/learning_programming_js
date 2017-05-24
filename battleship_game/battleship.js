acd80f27e30f8e57afcd55ed9c5a395206dbd468
Maksim Loginov <loginovmi@gmail.com>
2017-05-24 23:24:05 +1000

    added controller object

--- battleship_game/battleship.js
+++ battleship_game/battleship.js
@@ -93,6 +93,63 @@ var model = {
 
 };
 
+var controller = {
+    guesses: 0,
+    /**
+     * метод processGuess, получающий координаты в формате 'A0'.
+     * @param guess
+     */
+    processGuess: function (guess) {
+        // метод parseGuess будет использоваться для проверки введенных данных
+        var location = parseGuess(guess);
+        // если метод не возвращает null, значит был получен действительный объект location
+        if (location) {
+            // если полльзователь ввел правильные координаты, то счетчик увеличивается на 1,
+            // при вводе недействительных координат, попытка в подсчет не включается
+            this.guesses++;
+            var hit = model.fire(location);
+            // если выстрел попал в цель, а количество потопленных кораблей равно количеству кораблей в игре,
+            // выводится сообщение о том, что все корабли потоплены
+            if (hit && model.shipsSunk === model.numShips) {
+                view.displayMessage("You sank all my battleships, in " + this.guesses + " guesses");
+            }
+        }
+    }
+};
+/**
+ * Вспомогательная функция для проверки введенных значений пользователем
+ * @param guess
+ * @returns {*}
+ */
+function parseGuess(guess) {
+    // массив заполняется всеми буквами, которые могут присутствовать в действительных координатах
+    var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+
+    // проверяем данные на null и убеждаемся, что в строке два символа
+    if (guess === null || guess.length !== 2) {
+        alert("Oops, please enter a letter and a number on the board.");
+    } else {
+        // извлекаем первый символ строки
+        var firstChar = guess.charAt(0);
+        // при помощи метода indexOf получаем цифру в диапазоне от 0 до 6, соответствующую букве
+        var row = alphabet.indexOf(firstChar);
+        // получаем второй символ, представляющий столбец игрового поля
+        var column = guess.charAt(1);
+
+        // при помощи функция isNaN выявляем строки и столбцы, которые не являются цифрами
+        if (isNaN(row) || isNaN(column)) {
+            alert("Oops, that isn't on the board.");
+        } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) { // проверяем, что цифры лежат в диапазоне от 0 до 6
+            alert("Oops, that's off the board!");
+        } else {
+            // строка и столбец объединяются в строку, а результат возвращается методом
+            return row + column;
+        }
+    }
+    // если управление передано в эту точку, значит, какая-то проверка не прошла, и метод возвращает null
+    return null;
+}
+
 // view.displayMiss("00");
 // view.displayHit("34");
 // view.displayMiss("55");
@@ -101,13 +158,30 @@ var model = {
 // view.displayHit("26");
 // view.displayMessage("Tap tap, is this thing on?");
 
-model.fire("53");
-model.fire("06");
-model.fire("16");
-model.fire("26");
-model.fire("34");
-model.fire("24");
-model.fire("44");
-model.fire("12");
-model.fire("11");
-model.fire("10");
+// model.fire("53");
+// model.fire("06");
+// model.fire("16");
+// model.fire("26");
+// model.fire("34");
+// model.fire("24");
+// model.fire("44");
+// model.fire("12");
+// model.fire("11");
+// model.fire("10");
+
+// console.log(parseGuess("A0"));
+// console.log(parseGuess("B6"));
+// console.log(parseGuess("G3"));
+// console.log(parseGuess("H0"));
+// console.log(parseGuess("A7"));
+
+controller.processGuess("A0");
+controller.processGuess("A6");
+controller.processGuess("B6");
+controller.processGuess("C6");
+controller.processGuess("C4");
+controller.processGuess("D4");
+controller.processGuess("E4");
+controller.processGuess("B0");
+controller.processGuess("B1");
+controller.processGuess("B2");
