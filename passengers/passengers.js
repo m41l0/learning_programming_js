48fffa64827b8bcd58a876aa0d016313588156bd
Maksim Loginov <loginovmi@gmail.com>
2017-06-03 13:47:15 +1000

    edited passengers program

--- passengers/passengers.js
+++ passengers/passengers.js
@@ -1,15 +1,15 @@
 var passengers = [
-    {name: 'Jane Doloop', paid: true},
-    {name: 'Dr. Evel', paid: true},
-    {name: 'Sue Property', paid: false},
-    {name: 'John Funcall', paid: true}];
+    {name: 'Jane Doloop', paid: true, ticket: 'coach'},
+    {name: 'Dr. Evel', paid: true, ticket: 'supercoach'},
+    {name: 'Sue Property', paid: false, ticket: 'firstclass'},
+    {name: 'John Funcall', paid: true, ticket: 'coach'}];
 
-// функция провекри на присутствие в черном списке
+// проверка на присутствие в черном списке
 function checkNoFlyList(passenger) {
     return (passenger.name === 'Dr. Evel');
 }
 
-// функция провекри оплаты билетов
+// проверка оплаты билетов
 function checkNotPaid(passenger) {
     return (!passenger.paid);
 }
@@ -24,7 +24,7 @@ function processPassengers(passengers, testFunction) {
     return true;
 }
 
-// функция вывода списка пассажиров
+// вывод списка пассажиров
 function printPassenger(passenger) {
     var message = passenger.name;
     if (passenger.paid === true) {
@@ -34,6 +34,73 @@ function printPassenger(passenger) {
     }
     console.log(message);
     return false;
+}
+
+// обслуживание клиентов
+function serveCustomer(passenger) {
+    var getDrinkOrderFunction = createDrinkOrder(passenger);
+    var getDinnerOrderFunction = createDinnerOrderFunction(passenger);
+
+    // console.log('get - ' + getDrinkOrderFunction);
+    // console.log('create - ' + createDrinkOrder);
+
+    getDrinkOrderFunction();
+    // пердолжить обед
+    getDrinkOrderFunction();
+    getDinnerOrderFunction();
+    getDrinkOrderFunction();
+    // включить кино
+    getDrinkOrderFunction();
+    // забрать мусор
+}
+
+// перебираем пассажиров в массиве passengers и вызываем serveCustomer для каждого пассажира
+function servePassengers(passengers) {
+    for (var i = 0; i < passengers.length; i++) {
+        serveCustomer(passengers[i]);
+    }
+}
+
+// заказ напитков
+function createDrinkOrder(passenger) {
+    // cоздаем переменную для хранения функции, которую мы возвращаем
+    var orderFunction;
+
+    if (passenger.ticket === 'firstclass') {
+        orderFunction = function() {
+            alert('Would you like a cocktail or wine?');
+        }
+    } else if (passenger.ticket === 'coach') {
+        orderFunction = function() {
+            alert('Your choice is cola or water.');
+        }
+    } else {
+        orderFunction = function() {
+            alert('Your choice is cola, water or wine.');
+        }
+    }
+    return orderFunction;
+}
+
+// заказ обеда
+function createDinnerOrderFunction(passenger) {
+    // cоздаем переменную для хранения функции, которую мы возвращаем
+    var orderFunction;
+
+    if (passenger.ticket === 'firstclass') {
+        orderFunction = function() {
+            alert('Would you like a chicken or pasta?');
+        }
+    } else if (passenger.ticket === 'coach') {
+        orderFunction = function() {
+            alert('Your choice is nuts or crackers.');
+        }
+    } else {
+        orderFunction = function() {
+            alert('Your choice is snacks or cheese plate.');
+        }
+    }
+    return orderFunction;
 }
 
 var allCanFly = processPassengers(passengers, checkNoFlyList);
@@ -47,3 +114,5 @@ if (!allPaid) {
 }
 
 processPassengers(passengers, printPassenger);
+
+servePassengers(passengers);
